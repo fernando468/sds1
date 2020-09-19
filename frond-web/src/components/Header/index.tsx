55703f4d817ebeae35a76e622c0d9759a1c94a7d
fernando468 <fernando468@outlook.com>
2020-09-19 00:09:24 -0300

    Criando pagina Home

--- frond-web/src/components/Header/index.tsx
+++ frond-web/src/components/Header/index.tsx
@@ -3,13 +3,13 @@ import { ReactComponent as Logo } from '../../assets/logo.svg';
 import './styles.css';
 
 const Header = () => (
-    <header className="main-header">
-        <Logo />
-        <div className="logo-text">
-            <span className="logo-text-1">Big Game</span>
-            <span className="logo-text-2"> Survey</span>
-        </div>
-    </header>
+  <header className="main-header">
+    <Logo />
+    <div className="logo-text">
+      <span className="logo-text-1">Big Game</span>
+      <span className="logo-text-2"> Survey</span>
+    </div>
+  </header>
 );
 
 export default Header;
